9fdd8b679a24d84eaabb9e2f81bbbfc166c44ab3
Javed Ramjohn <ramjohnja13@gmail.com>
2016-11-05 18:29:01 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,21 @@ Rails.application.routes.draw do
 
  # root to: "home#show"
   root "welcome#about"
+
+  get '/edit', to: "users#edit"
   
   get '/signup', to: "registrations#new"
-
   post '/signup', to: "users#create"
-
-  post '/signup', to: "registrations#create"
 
   get 'login', to: "sessions#new"
   post '/login', to: "sessions#create" 
   delete '/logout', to: "sessions#destroy"
 
-  get 'users/:id', to: "users#show", as: "profile"
-
   resources :chatrooms, param: :slug
   resources :messages
+  resources :users
+  
+  get 'users/:id', to: "users#show", as: "profile"
   
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
