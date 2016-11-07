649eb48cc452eae3b209bd8b81358ed9a83f5fa3
Javed Ramjohn <ramjohnja13@gmail.com>
2016-11-06 23:52:01 -0500

    Chatroom Authorization Implemented. Chatroom edit implemented with relevant authorization. Escalation implemented. Chatroom archive implemented.

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,7 +2,7 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
-  validates :topic, presence: true, uniqueness: true, case_sensitive: false
+  validates :topic, presence: true, uniqueness: false, case_sensitive: false
   validates :user_id, presence: true
   before_validation :sanitize, :slugify
 
